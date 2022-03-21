1bbaac65b7006dd472117584ddba58b4fd429149
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2022-03-21 19:29:16 +0100

    skeleton SQL question

--- Insertions_lignes_BoatBooking_Hoareau_Diallo_Aittahar.sql
+++ Insertions_lignes_BoatBooking_Hoareau_Diallo_Aittahar.sql
@@ -1,1 +1,8 @@
 -- Insertions_lignes_BoatBooking_Hoareau_Diallo_Aittahar.sql
+
+
+----------------TABLE : BATEAU----------------
+INSERT INTO ...
+
+----------------TABLE : BATEAU----------------
+INSERT INTO ...
--- Schema_physique_BoatBooking_Hoareau_Diallo_Aittahar.sql
+++ Schema_physique_BoatBooking_Hoareau_Diallo_Aittahar.sql
@@ -1,1 +1,21 @@
 -- Schema_physique_BoatBooking_Hoareau_Diallo_Aittahar.sql
+
+
+----------------TABLE : XXXXX----------------
+
+CREATE TABLE schema_name.table_name (
+    column_1 data_type column_constraint,
+    column_2 data_type column_constraint,
+    ...
+    table_constraint
+ );
+
+
+ ----------------TABLE : XXXXX----------------
+
+CREATE TABLE schema_name.table_name (
+    column_1 data_type column_constraint,
+    column_2 data_type column_constraint,
+    ...
+    table_constraint
+ );
