e13f5974469b547cbd29fa4f66c7a2a1896d325b
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2022-04-08 22:36:05 +0200

    #14 add 3 INSERT INTO for BATEAU

--- Insertions_lignes_BoatBooking_Hoareau_Diallo_Aittahar.sql
+++ Insertions_lignes_BoatBooking_Hoareau_Diallo_Aittahar.sql
@@ -1,6 +1,16 @@
 -- Insertions_lignes_BoatBooking_Hoareau_Diallo_Aittahar.sql
 
 
+----------------TABLE : BATEAU----------------
+-- INSERT INTO BATEAU VALUES (MATRICULE,NOM,COULEUR,VITESSE_MAX,LITRE CARBURANT HEURE,TYPE CARBURANT,MAX_PERSONNE,DESCRIPTION,UTILISABLE,NOTE,LONG,LARG,ANNEE,ETAT) --Marque: XXXX & Type : XXX
+INSERT INTO BATEAU VALUES ('MAF43878','Hunter Channel 32','blanc',80,0.8,'Diesel',5,NULL,1,NULL,9.75,3.12,1993,'Neuf'); --Marque: Hunter & Type : Voile
+INSERT INTO BATEAU VALUES ('RUF43931','Nicholson 303','rouge',110,0.8,'Diesel',5,'Le Nicholson 303 offre cinq couchettes dans deux cabines avec une salle d''eau séparée entre la cabine avant et le carré principal.',0,'Bateau en mauvaise état et besoin de réparation ',9.14,3.11,1979,'Mauvais') --Marque: Nicholson & Type : Cabine
+INSERT INTO BATEAU VALUES ('MAF20870','Wauquiez Pilot Saloon 48','blanc',130,0.9,'Diesel',8,'Un croiseur d''eau bleue parfait. Le Pilot Saloon 48 est la quintessence du savoir-faire Wauquiez en matière de conception et de construction de yachts haut de gamme.',1,NULL,14.77,4.61,2022,'Neuf') --Marque: Wauquiez & Type : Moteur
+
+
+----------------TABLE : XXX----------------
+INSERT INTO ...
+
 ----------------TABLE : XXX----------------
 INSERT INTO ...
 
