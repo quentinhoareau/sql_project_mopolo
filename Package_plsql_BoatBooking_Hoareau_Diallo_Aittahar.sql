ceeeb6855b3bf9d307a63f6ec1ee883e975495c5
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2022-04-21 10:50:56 +0200

    fix function appelation + change function to RESERVATION

--- Package_plsql_BoatBooking_Hoareau_Diallo_Aittahar.sql
+++ Package_plsql_BoatBooking_Hoareau_Diallo_Aittahar.sql
@@ -15,16 +15,16 @@ CREATE OR REPLACE PACKAGE BATEAU_PACKAGE AS
    PROCEDURE supprimerBateau(...); 
 
    --Consulation
-   FUNCTION prixBateau(...); 
+   FUNCTION consulterBateau(...); 
 
-   --Mise à jours
+   -- Mise à jours
    PROCEDURE majBateau(...); 
   
 END BATEAU_PACKAGE; 
 /
 
 CREATE OR REPLACE PACKAGE BODY BATEAU_PACKAGE AS 
-   PROCEDURE ajouterBateau(...) x
+   PROCEDURE ajouterBateau(...)
    IS 
    BEGIN 
       ...
@@ -36,11 +36,12 @@ CREATE OR REPLACE PACKAGE BODY BATEAU_PACKAGE AS
       ...
    END supprimerBateau; 
 
-   FUNCTION prixBateau(...) 
+   FUNCTION consulterBateau(...)  RETURN ...
    IS 
    BEGIN 
       ...
-   END prixBateau; 
+   END consulterBateau; 
+    
 
    PROCEDURE majBateau(...) 
    IS 
@@ -50,7 +51,7 @@ CREATE OR REPLACE PACKAGE BODY BATEAU_PACKAGE AS
    
    
 END BATEAU_PACKAGE; 
-/
+
 
 
 
@@ -86,7 +87,40 @@ END BATEAU_PACKAGE;
 /*
 /*==============================================================*/
 
-...
+CREATE OR REPLACE PACKAGE RESERVATION_PACKAGE AS 
+
+   --Consulation
+   FUNCTION prixReservation(bat_imma reservation.BT_IMMATRICULE%type, date_deb reservation.RES_DATE_DEBUT%type) RETURN number; 
+
+END RESERVATION_PACKAGE; 
+/
+
+CREATE OR REPLACE PACKAGE BODY RESERVATION_PACKAGE AS 
+
+   FUNCTION prixReservation(bat_imma reservation.BT_IMMATRICULE%type, date_deb reservation.RES_DATE_DEBUT%type) RETURN number
+   IS 
+        nbReservation number;
+        prix number;
+   BEGIN 
+      SELECT COUNT(*) INTO nbReservation FROM RESERVATION WHERE BT_IMMATRICULE = bat_imma AND RES_DATE_DEBUT = date_deb;
+      IF( nbReservation = 1 ) THEN
+      SELECT 1 INTO prix FROM RESERVATION WHERE BT_IMMATRICULE = bat_imma AND RES_DATE_DEBUT = date_deb;
+      ELSE
+         RAISE_APPLICATION_ERROR(-20001,'Aucune réservation n''est enregistré pour ce bateau : "'||bat_imma||'", impossible de calculer le prix.');
+      END IF;
+      RETURN prix;
+   END prixReservation; 
+
+END RESERVATION_PACKAGE; 
+/
+
+DECLARE 
+   prix number; 
+BEGIN 
+   prix := RESERVATION_PACKAGE.prixReservation('MAF43878',TO_DATE('2022-04-20 09:00:00','yyyy-mm-dd hh24:mi:ss')); 
+   dbms_output.put_line('Prix de la réservation: ' || prix); 
+END; 
+/
 
 
 /*==============================================================*/
